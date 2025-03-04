47bafd846fffe1aeb53e06ee23d723947b095c28
Max Barren <csgold2002@mail.ru>
2025-03-04 22:32:32 +0300

    added connection to the azure

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { SongsService } from './songs.service'
 import { SongsController } from './songs.controller'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { HttpModule } from '@nestjs/axios'
 
 @Module({
+	imports: [HttpModule],
 	controllers: [SongsController],
 	providers: [SongsService, PrismaService]
 })
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,51 +1,76 @@
+import { HttpService } from '@nestjs/axios'
 import { BadRequestException, Injectable } from '@nestjs/common'
+import { env } from 'process'
+import { lastValueFrom } from 'rxjs'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class SongsService {
-	constructor(private readonly prisma: PrismaService) {}
-	private readonly songs = [
-		{
-			id: '123rtu9jfksljfsef-213jfskdfse32-314234f',
-			thumbnail: 'thumbnails/cover-2.webp',
-			title: 'waaaaaaavyyyyyy',
-			author: 'Oliver Francis ~',
-			file: '/',
-			likes: 5
-		},
-		{
-			id: 'fsgor-zxc-ajnfljakgb',
-			thumbnail: 'thumbnails/cover-5.webp',
-			title: 'Mob Depp',
-			author: 'Frank Sinatra',
-			file: '/',
-			likes: 41
-		}
-	]
+	constructor(
+		private readonly prisma: PrismaService,
+		private readonly httpService: HttpService
+	) {}
+	// private readonly songs = [
+	// 	{
+	// 		id: '123rtu9jfksljfsef-213jfskdfse32-314234f',
+	// 		thumbnail: 'thumbnails/cover-2.webp',
+	// 		title: 'waaaaaaavyyyyyy',
+	// 		author: 'Oliver Francis ~',
+	// 		file: '/',
+	// 		likes: 5
+	// 	},
+	// 	{
+	// 		id: 'fsgor-zxc-ajnfljakgb',
+	// 		thumbnail: 'thumbnails/cover-5.webp',
+	// 		title: 'Mob Depp',
+	// 		author: 'Frank Sinatra',
+	// 		file: '/',
+	// 		likes: 41
+	// 	}
+	// ]
 
-	private readonly currentTrack = {
-		id: 1,
-		thumbnail: 'thumbnails/cover-2.webp',
-		title: 'waaaaaaavyyyyyy',
-		author: 'Oliver Francis ~',
-		file: 'files/wavy.mp3',
-		likes: 5
-	}
+	// private readonly currentTrack = {
+	// 	id: 1,
+	// 	thumbnail: 'thumbnails/cover-2.webp',
+	// 	title: 'waaaaaaavyyyyyy',
+	// 	author: 'Oliver Francis ~',
+	// 	file: 'files/wavy.mp3',
+	// 	likes: 5
+	// }
 
 	async connect() {
-		// return this.currentTrack
-		const currentSong = await this.prisma.song.findFirst({
-			select: {
-				author: true,
-				file: true,
-				id: true,
-				thumbnail: true,
-				title: true
-			}
-		})
-		if (!currentSong) throw new BadRequestException('Something went wrong')
+		// const currentSong = await this.prisma.song.findFirst({
+		// 	select: {
+		// 		author: true,
+		// 		file: true,
+		// 		id: true,
+		// 		thumbnail: true,
+		// 		title: true
+		// 	}
+		// })
+		// if (!currentSong) throw new BadRequestException('Something went wrong')
 
-		return currentSong
+		// return currentSong
+
+		const currentSong = await lastValueFrom(
+			this.httpService.get(process.env.STATION_URL)
+		).then(res => res.data)
+
+		// return currentSong
+
+		return {
+			url: currentSong.station.listen_url,
+			currentListeners: currentSong.listeners.current,
+			song: {
+				id: currentSong.now_playing.song.id,
+				playedAt: currentSong.now_playing.played_at,
+				duration: currentSong.now_playing.duration,
+				elapsed: currentSong.now_playing.elapsed,
+				thumbnail: currentSong.now_playing.song.art,
+				title: currentSong.now_playing.song.text || 'Unknown',
+				author: currentSong.now_playing.song.artist || 'Unknown'
+			}
+		}
 	}
 
 	async isSongLiked(userId: string, songId: number) {
@@ -146,20 +171,20 @@ export class SongsService {
 			)
 
 		await this.prisma.$transaction([
-				this.prisma.likedSong.delete({
-					where: {
-						userId_songId: {
-							userId: userId,
-							songId: songId
-						}
+			this.prisma.likedSong.delete({
+				where: {
+					userId_songId: {
+						userId: userId,
+						songId: songId
 					}
-				}),
-				this.prisma.song.update({
-					where: { id: songId },
-					data: { likes: { decrement: 1 } }
-				})
-			])
-		
+				}
+			}),
+			this.prisma.song.update({
+				where: { id: songId },
+				data: { likes: { decrement: 1 } }
+			})
+		])
+
 		return {}
 	}
 }
